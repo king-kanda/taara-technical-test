049228636c0183c95906de92b12b9f3cdb93e370
king-kanda <stevenokanda@gmail.com>
2024-11-17 19:30:26 +0300

    firebase setup

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZSR9HSfyp2Sa9tR82SwYOCI45ZLo1e8M",
+  authDomain: "laara-technical.firebaseapp.com",
+  projectId: "laara-technical",
+  storageBucket: "laara-technical.firebasestorage.app",
+  messagingSenderId: "1041657511551",
+  appId: "1:1041657511551:web:3430ebe590995bb472c94a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
